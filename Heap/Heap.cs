3f4c1ba619b67c553f5b38f95f67927b06119934
oleksandr.shevchuk222@gmail.com <oleksandr.shevchuk222@gmail.com>
2022-08-10 14:30:18 +0300

    Add project files.

---
+++ Heap/Heap.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Heap
+{
+    internal class Heap : IEnumerable
+    {
+        private List<int> items = new List<int>();
+        public int Count => items.Count;
+        public Heap() { }
+        public Heap(List<int> items)
+        {
+            this.items.AddRange(items);
+            for (int i = Count; i >= 0; i--)
+            {
+                Sort(i);
+            }
+        }
+        public int? Peek()
+        {
+            if (Count > 0)
+            {
+                return items[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void Add(int item)
+        {
+            items.Add(item);
+            var currentIndex = Count - 1;
+            var parentIndex = GetPerentIndex(currentIndex);
+
+            while (currentIndex > 0 && items[parentIndex] < items[currentIndex])
+            {
+                Swap(currentIndex, parentIndex);
+
+                currentIndex = parentIndex;
+                parentIndex = GetPerentIndex(currentIndex);
+            }
+        }
+        public int GetMax()
+        {
+            var result = items[0];
+            items[0] = items[Count - 1];
+            items.RemoveAt(Count - 1);
+            Sort(0);
+            return result;
+        }
+
+        private void Sort(int currentIndex)
+        {
+            int maxIndex, leftIndex, rightIndex;
+            maxIndex = currentIndex;
+
+            while (currentIndex < Count)
+            {
+                leftIndex = currentIndex*2 + 1;
+                rightIndex = currentIndex*2 + 2;
+
+                if (leftIndex < Count && items[leftIndex] > items[maxIndex])
+                {
+                    maxIndex = leftIndex;
+                }
+                if (rightIndex < Count && items[rightIndex] > items[maxIndex])
+                {
+                    maxIndex = rightIndex;
+                }
+                if (maxIndex == currentIndex)
+                {
+                    break;
+                }
+                Swap(currentIndex, maxIndex);
+                currentIndex = maxIndex;
+            }
+        }
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            var temp = items[currentIndex];
+            items[currentIndex] = items[parentIndex];
+            items[parentIndex] = temp;
+        }
+
+        private static int GetPerentIndex(int currentIndex)
+        {
+            return (currentIndex - 1) / 2;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (Count > 0)
+            {
+                yield return GetMax();
+            }
+        }
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Heap
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Stopwatch();
+            //heap.Add(20);
+            //heap.Add(11);
+            //heap.Add(17);
+            //heap.Add(7);
+            //heap.Add(4);
+            //heap.Add(13);
+            //heap.Add(15);
+            //heap.Add(14);
+            var rnd = new Random();
+            var startItems = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                startItems.Add(rnd.Next(-1000, 1000));
+            }
+            timer.Start();
+            var heap = new Heap(startItems);
+            timer.Stop();
+            Console.WriteLine("1) Add 1000 elements: " + timer.Elapsed);
+
+            timer.Restart();
+            for (int i = 0; i < 100; i++)
+            {
+                heap.Add(rnd.Next(-1000, 1000));
+            }
+            timer.Stop();
+            Console.WriteLine("2) Add 1000 elements: " + timer.Elapsed);
+
+            timer.Restart();
+            foreach (var item in heap)
+            {
+                Console.WriteLine(item);
+            }
+            timer.Stop();
+            Console.WriteLine("3) Console 2000 elements: " + timer.Elapsed);
+        }
+    }
+}
